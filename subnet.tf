cbbf9678961447114bc6e9c3ea37e3f2fc844eb1
bhardwaj-addweb <bhardwaj@addwebsolution.in>
2024-02-15 18:39:08 +0530

    initial commint

--- subnet.tf
+++ subnet.tf
@@ -6,3 +6,21 @@ resource "aws_subnet" "main" {
     Name = "bhardwaj-test"
   }
 }
+
+resource "aws_subnet" "main1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.16/28"
+
+  tags = {
+    Name = "bhardwaj-test-1"
+  }
+}
+
+resource "aws_subnet" "main2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.32/28"
+
+  tags = {
+    Name = "bhardwaj-test-2"
+  }
+}
